beb4ac5d10f56754d8ecd9d91ea4f1fe7f93587a
VedantBangoria <vedantbangoria@gmail.com>
2025-10-08 22:21:36 -0400

    Initial project setup with game source files
    Added Eclipse project configuration, .gitignore files, and initial game source files including Asteroid, AsteroidDodger, Game, Point, and Polygon classes. This sets up the basic structure for the Asteroid Dodger game implementation. Working implementation of asteroid class can be found in the AsteroidDodger class and can be ran to see behavior of asteroid

---
+++ YOURGAMENAMEHERE/src/game/Asteroid.java
@@ -0,0 +1,43 @@
+package game;
+
+import java.awt.Color;
+
+public class Asteroid{
+	
+	private Polygon shape;
+	private final Color COLOR = Color.GRAY;
+	private int xSpeed;
+	private int ySpeed;
+	
+	public Asteroid(int x, int y, int xSpeed, int ySpeed) {
+		
+		Point[] points = {new Point(0, 10), new Point(7, 0), new Point(17, 0), new Point(24, 10), new Point(24, 20), new Point(17, 30), new Point(7, 30), new Point(0, 20)};
+		this.shape = new Polygon(points, new Point(x, y), 0);
+		this.xSpeed = xSpeed;
+		this.ySpeed = ySpeed;
+	}
+	
+	public Polygon getShape() {
+		return shape;
+	}
+	
+	public Color getColor() {
+		return this.COLOR;
+	}
+	
+	public void move(int screenWidth, Point reset) {
+        shape.position.x += xSpeed;
+        shape.position.y += ySpeed;
+        if (shape.position.x > screenWidth || shape.position.x < 0) {
+        	//shape.position.x = reset.getX();
+        	//shape.position.y = reset.getY();
+        	xSpeed *= -1;
+        } 
+        
+        if(shape.position.y > screenWidth || shape.position.y < 0) {
+        	ySpeed *= -1;
+        }
+       
+    }
+	
+}
