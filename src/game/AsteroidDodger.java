2b396ebcdce8ad768e2a8ae14f8206a4ce79ec60
VedantBangoria <vedantbangoria@gmail.com>
2025-10-13 16:11:29 -0400

    Update AsteroidDodger.java with game implementation. The asteroids stay locked in position if they collide with the player, forcing the player to move quickly unless they want their health drained very quickly

--- src/game/AsteroidDodger.java
+++ src/game/AsteroidDodger.java
@@ -9,52 +9,102 @@ NOTE: This class is the metaphorical "main method" of your program,
 */
 import java.awt.*;
 import java.awt.event.*;
+import java.util.Random;
 
 class AsteroidDodger extends Game {
 	static int counter = 0;
 	static int rotation = 0;
 	//keep asteroid instances outside of paint method for rotation
-	private Asteroid a;
+	private Asteroid[] asteroids;
 	private Player p;
+	
 	
   public AsteroidDodger() {
     super("YourGameName!",1000,1000);
     this.setFocusable(true);
 	this.requestFocus();
-	this.a = new Asteroid(200, 200, 16, 8);
-	this.p = new Player(500,500,5,15);
+	this.asteroids = new Asteroid[] {new Asteroid(100, 150, 5, 7), new Asteroid(300, 400, 7, 7),
+		  /*  new Asteroid(500, 250, 23, 18),
+		    new Asteroid(800, 600, 28, 16),
+		    new Asteroid(200, 700, 17, 21),
+		    new Asteroid(900, 300, 30, 15),
+		    new Asteroid(400, 850, 25, 19),
+		    new Asteroid(600, 500, 29, 17),
+		    new Asteroid(750, 200, 32, 14), 
+		    new Asteroid(100, 900, 26, 18) */
+		    };
+	
+	this.p = new Player(500,500,100,15);
+	
+	
 	this.addKeyListener(this.p);
   }
   
-	public void paint(Graphics brush) {
-    	brush.setColor(Color.black);
+  
+  	public void paint(Graphics brush) {
+  		brush.setColor(Color.black);
     	brush.fillRect(0,0,width,height);
-    	
-    	// sample code for printing message for debugging
-    	// counter is incremented and this message printed
-    	// each time the canvas is repainted
-    	counter++;
-    	brush.setColor(Color.white);
-    	
+  		if(!p.isGameOver()) {
+  			
+  			if(collisionCheck()) {
+  				p.decrementHealth();
+  			} else {
+  				moveAsteroids();
+  				
+  			}
 
-            // Apply starting position and rotation
-    		
-            Polygon shape = a.getShape();
-            brush.setColor(a.getColor());
-            
-            shape.rotation += 5;
-            
-            
-            draw(shape.getPoints(), brush);
+  	        // Apply damage only if player is not invincible
+  	        
+  			
+  			if(p.getHealth() <= 0) {
+  				p.setGameOver();
+  			}
+  			
+  			
+  			
+  			p.move();
+  			
+  			brush.setColor(Color.GRAY);
+  			for(Asteroid a : asteroids) {
+  				a.getShape().rotation+=5;
+  				draw(a.getShape().getPoints(), brush);
+  			}
+  			
+  			brush.setColor(Color.GRAY);
             draw(p.getShape().getPoints(), brush);
-            a.move(1000, new Point(50, 200));
             
-
-    	
-    	
-        
-    	brush.drawString("Counter is " + counter,10,10);
-  }
+  			if(counter%60 == 0) {
+            	p.incrementTimer();
+            }
+  			
+  			 brush.drawString("time lived: " + p.getTimeLived(), 10, 50);
+  	        brush.drawString("lives: " + p.getHealth(), 10, 30);
+  	    	brush.drawString("Counter is " + counter,10,10);
+  			
+  		} else {
+  			brush.setColor(Color.WHITE);
+  			brush.drawString("Game Over", 500, 500);
+  		}
+  	}
+  	
+  	
+  	private  boolean collisionCheck() {
+  		for(Asteroid a : this.asteroids) {
+  			if(a.getShape().collides(p.getShape())) {
+  				return true;
+  			}
+  		}
+  		return false;
+  	}
+  	
+  	private void moveAsteroids() {
+  		for(Asteroid a : asteroids) {
+  			a.move(1000, p.getShape());
+  			
+  		}
+  	}
+  	
+	
 	
 	public static void draw(Point[] pts, Graphics brush) {
 		 int[] x = new int[pts.length];
@@ -67,6 +117,12 @@ class AsteroidDodger extends Game {
             brush.fillPolygon(x, y, pts.length);
 	}
   
+	
+	
+	
+	
+	
+	
 	public static void main (String[] args) {
    		AsteroidDodger a = new AsteroidDodger();
 		a.repaint();
